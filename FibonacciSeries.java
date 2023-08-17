32f3b5703737f412c1a58c6e4df0dfde232bc1ef
ParkaviDevarajan <142524062+ParkaviDevarajan@users.noreply.github.com>
2023-08-18 02:13:12 +0530

    Week1.Day2

---
+++ FibonacciSeries.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+		
+		int num1=0,num2=1,num3;
+		for(int i=0;i<=8;i++)
+		{
+			num3=num1+num2;
+			System.out.println(num3);
+			num1=num2;
+			num2=num3;
+					
+
+	}
+	}
+}
