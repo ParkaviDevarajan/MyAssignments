86ad3c98d6bddb113cad37421a649c630014cea9
ParkaviDevarajan <142524062+ParkaviDevarajan@users.noreply.github.com>
2023-08-18 01:50:41 +0530

    Week1.Day1

---
+++ NegativeToPositive.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class NegativeToPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=-5;
+		System.out.println("The given Negative number is:"+n);
+		
+		int pos=Math.abs(n);
+		
+		System.out.println("The converted Positive number is:"+pos);
+
+	}
+
+}
---
+++ PositiveNegative.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=-5;
+		if(num>0) {
+			System.out.println("The given number is Positive");
+				}
+		else
+			System.out.println("The given number is negative");
+			
+       
+	}
+
+}
